0564fa9a53a6c6bf510f3f1d3cc3108a96262b95
sahilm0081 <freelancer0081@gmail.com>
2024-06-02 12:28:14 +0530

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -5,22 +5,22 @@ echo 'into the local Maven repository, which will ultimately be stored in'
 echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
 echo 'volume).'
 set -x
-mvn jar:jar install:install help:evaluate -Dexpression=project.name
+mvn jar:jar install:install help:evaluate -Dexpression=app
 set +x
 
 echo 'The following command extracts the value of the <name/> element'
 echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
 set -x
-NAME=`mvn -q -DforceStdout help:evaluate -Dexpression=project.name`
+NAME=`mvn -q -DforceStdout help:evaluate -Dexpression=app`
 set +x
 
 echo 'The following command behaves similarly to the previous one but'
 echo 'extracts the value of the <version/> element within <project/> instead.'
 set -x
-VERSION=`mvn -q -DforceStdout help:evaluate -Dexpression=project.version`
+VERSION=`mvn -q -DforceStdout help:evaluate -Dexpression=v1`
 set +x
 
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-java -jar target/app-v2.jar
+java -jar target/${NAME}-${VERSION}.jar
